ab5419a5e5a1a1d6d77b16e08b5a3b1a99809fda
yaelahbarr <wdeaddgaming@gmail.com>
2023-11-03 17:51:07 +0700

    video ke 8

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -11,4 +12,8 @@ class Post extends Model
 
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
 <article>
     <h2>{{ $post["title"] }}</h2>
+    <p> By. AlBarra Permana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post["body"] !!}
 </article>
 
